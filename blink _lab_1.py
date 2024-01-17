d9c470ecb71152c647d8d33641c472b3655a4a31
Elver0 <156936073+Elver0@users.noreply.github.com>
2024-01-17 08:34:23 -0800

    Create blink _lab_1.py

---
+++ blink _lab_1.py
@@ -0,0 +1,10 @@
+#import time
+from time to import sleep
+from ada_fruitdataplayground import cp
+
+(print("hello dusan")
+ while True:
+   cp.red_LED=true
+   sleep(0.5)
+   cp.red_led=false
+   sleep(0.5)
